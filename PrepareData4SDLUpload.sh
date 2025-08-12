76921f6d406690f55e4915a4c92f1f9703bee845
Iris Christadler <105636812+christadler@users.noreply.github.com>
2025-08-12 15:56:45 +0200

    Updated for 100runs catalog

--- PrepareData4SDLUpload.sh
+++ PrepareData4SDLUpload.sh
@@ -11,12 +11,13 @@
 #                  ...
 
 # Path to directory with input and slurm files 
-input_dir=/hppfs/work/pn49ha/di35ban/AltoTiberina
+#input_dir=/hppfs/work/pn49ha/di35ban/AltoTiberina
 # Path to directory with outputs
-output_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/outputs_10bigRuns
+#output_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/outputs_10bigRuns
+output_dir=/hppfs/scratch/0C/ra35zih2/AltoTiberina/outputs/
 # Path to sdl directory
-#sdl_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/sdl_10bigRuns
-sdl_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/sdl_10bigRuns_wSmallerFaultOutp
+#sdl_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/sdl_10bigRuns_wSmallerFaultOutput
+sdl_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/sdl_100Runs
 mkdir -pv $sdl_dir
 
 
@@ -24,16 +25,17 @@ mkdir -pv $sdl_dir
 # ############################################## #
 #   Copy input data that should be saved         #
 # ############################################## #
-cd $input_dir; echo "input_dir: "; pwd
-mkdir -pv $sdl_dir/inputs
-for i in {0000..0009}; do
-        # Copy input parameter files
-        cp -r $i   $sdl_dir/inputs/
-done
-# Copy slurm file + stdout + stderr
-cp *err    $sdl_dir/inputs/
-cp *out    $sdl_dir/inputs/
-cp *slurm  $sdl_dir/inputs/
+# The SDL wizard currently cannot handle the input data
+#cd $input_dir; echo "input_dir: "; pwd
+#mkdir -pv $sdl_dir/inputs
+#for i in {0000..0009}; do
+        ## Copy input parameter files
+        #cp -r $i   $sdl_dir/inputs/
+#done
+## Copy slurm file + stdout + stderr
+#cp *err    $sdl_dir/inputs/
+#cp *out    $sdl_dir/inputs/
+#cp *slurm  $sdl_dir/inputs/
 
 
 # ############################################## #
@@ -41,7 +43,7 @@ cp *slurm  $sdl_dir/inputs/
 # ############################################## #
 cd $output_dir; echo "output_dir: "; pwd
 # Adjust start to end number accordingly 0000..0099 or 0000..0009
-for i in {0000..0009}; do
+for i in {0000..0099}; do
         echo "ID: $i"
         cd $i; pwd
         mkdir -pv $sdl_dir/$i
@@ -72,11 +74,8 @@ for i in {0000..0009}; do
         # We need the fault information, however the directories 
         # fault_cell (11GB) and fault_vertex (25MB) are really big so 
         # we decided to keep only every 5th second
-        # cp $i-fault.xdmf $sdl_dir/$i
-        # cp -r $i-fault_cell $sdl_dir/$i
-        # cp -r $i-fault_vertex $sdl_dir/$i
-        cp $i\_5s-fault.xdmf $sdl_dir/$i
-        cp $i\_5s-fault.h5 $sdl_dir/$i
+        cp $i\_1s-fault.xdmf $sdl_dir/$i
+        cp $i\_1s-fault.h5 $sdl_dir/$i
 
         echo "cd .."
         cd ..; pwd
