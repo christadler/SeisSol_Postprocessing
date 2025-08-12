e2d4c56e24c09244fd0a1f4f29c03d9f77bcae49
Iris Christadler <105636812+christadler@users.noreply.github.com>
2025-08-12 12:22:26 +0200

    fault output every second

--- Step3.sh
+++ Step3.sh
@@ -16,14 +16,15 @@
 # Path to directory with outputs
 # output_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/outputs_test_step3
 # output_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/outputs
-output_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/outputs_10bigRuns
+#output_dir=/hppfs/scratch/0A/di35ban/AltoTiberina/outputs_10bigRuns
+output_dir=/hppfs/scratch/0C/ra35zih2/AltoTiberina/outputs
 
 # ############################################## #
 #            Postprocessing                      #
 # ############################################## #
 cd $output_dir; pwd
 # Adjust start to end number accordingly 0000..0099 or 0000..0009
-for i in {0000..0009}; do
+for i in {0000..0099}; do
         cd $i; pwd
         myfile=$i-surface.xdmf; ls -al $myfile
 #       ########################################################### #
@@ -43,7 +44,7 @@ for i in {0000..0009}; do
 #       Postprocessing Step 3: Shrink fault output (to every 5s)    #
 #       ########################################################### #
         echo "Postprocessing Step 3: Shrink fault output (to every 5s)"
-        seissol_output_extractor $i-fault.xdmf --var ASl Vr SRd SRs Sls Sld Pn0 Ts0 Td0 Mud PSR --time 0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90 --add2prefix _5s
+        seissol_output_extractor $i-fault.xdmf --var ASl Vr SRd SRs Sls Sld Pn0 Ts0 Td0 Mud PSR --time 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90  --add2prefix _5s
 
         cd ..; pwd
 done
